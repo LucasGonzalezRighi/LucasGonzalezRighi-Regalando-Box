0c68a2fabf5811dd807e85ec6ceb7758f9b89bd9
LucasGonzalezRighi <lucasgonzalezrighi@gmail.com>
2022-02-10 22:08:02 -0300

    prueba 2.0

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -27,7 +27,7 @@ export default  function NavBar(){
                         <a className="nav-link" href="productos.html">Sobre nuestros productos</a>
                         <a className="nav-link" href="tienda.html">Tienda</a>
                         <a className="nav-link" href="refill.html">Refill</a>
-                        <p>hola mundo</p>
+                        <p>hola mundo soy lucas</p>
                     </div>
                 </div>
                 <CartWidget />
